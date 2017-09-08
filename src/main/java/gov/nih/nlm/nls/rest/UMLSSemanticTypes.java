62f5eb2190c71316ef6d7a164aa791423e4d2ff0
Willie Rogers <wjrogers@nih.gov>
2017-09-08 11:17:47 -0400

    UMLS Semantic Types class

---
+++ src/main/java/gov/nih/nlm/nls/rest/UMLSSemanticTypes.java
@@ -0,0 +1,85 @@
+package gov.nih.nlm.nls.rest;
+
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Describe class UMLSSemanticTypes here.
+ *
+ *
+ * Created: Thu Sep  7 12:39:42 2017
+ *
+ * @author <a href="mailto:wjrogers@mail.nih.gov">Willie Rogers</a>
+ * @version 1.0
+ */
+public class UMLSSemanticTypes {
+  /** log4j logger instance */
+  private static final Logger logger = LogManager.getLogger(UMLSSemanticTypes.class);
+
+  String listFilename = "data/SemanticTypes_2013AA.txt";
+  
+  public class TypeRecord {
+    /** semantic type abbreviation */
+    String abbrev;
+    /** semantic type id */
+    String id;
+    /** semantic type full name */
+    String name;
+
+    public TypeRecord(String abbrev, String id, String name) {
+      this.abbrev = abbrev;
+      this.id = id;
+      this.name = name;
+    }
+    public String getAbbrev() { return this.abbrev; }
+    public String getId() { return this.id; }
+    public String getName() { return this.name; }
+    public String toString() {
+      return this.abbrev + "|" +
+	this.id + "|" +
+	this.name + "|";
+    }
+  }
+
+  /** semantic type list */
+  List<TypeRecord> semanticTypeList = new ArrayList();
+
+  /**
+   * Creates a new <code>UMLSSemanticTypes</code> instance.
+   *
+   */
+  public UMLSSemanticTypes(String filename) {
+    loadTable(filename);
+  }
+
+  public void loadTable(String filename) {
+    try {
+      BufferedReader br = new BufferedReader(new FileReader(filename));
+      String line;
+      while ((line = br.readLine()) != null) {
+	String[] fields = line.split("\\|");
+	TypeRecord typeRecord = 
+	  new TypeRecord(fields[0], fields[1], fields[2]);
+	this.semanticTypeList.add(typeRecord);
+	logger.info(typeRecord);
+      }
+      br.close();
+    } catch (FileNotFoundException fnfe) {
+	throw new RuntimeException(fnfe);
+    } catch (IOException ioe) {
+      	throw new RuntimeException(ioe);
+    }
+  }
+
+  public List<TypeRecord> getSemanticTypeList() {
+    return this.semanticTypeList;
+  }
+
+}
